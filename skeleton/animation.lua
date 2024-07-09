a3bf64ee5f5f174ff8a0602fd3d15f227cb67ab6
Finkch <skyler@thealdersons.org>
2024-07-09 15:43:15 -0700

    Animation now has attribute

--- skeleton/animation.lua
+++ skeleton/animation.lua
@@ -7,9 +7,10 @@ Animation = {}
 Animation.__index = Animation
 Animation.__type = "animation"
 
-function Animation:new()
+function Animation:new(keyframes)
     local a = {
-
+        keyframes = keyframes,  -- an ordered list of all keyframes in the animation
+        
     }
 
     -- keyframes
