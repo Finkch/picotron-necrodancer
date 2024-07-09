f195952582753fa6b3fea17ce93b7d6e258afdf1
Finkch <skyler@thealdersons.org>
2024-07-09 14:44:30 -0700

    Now have simple skeleton with only a hip

---
+++ graveyard.lua
@@ -0,0 +1,32 @@
+--[[
+    a place to find skeletons.
+
+    what i mean is you load skeleton and animation data from here.
+
+]]
+
+include("lib/vec.lua")
+
+include("skeleton/skeleton.lua")
+include("skeleton/bone.lua")
+include("skeleton/necromancer.lua")
+include("skeleton/animation.lua")
+include("skeleton/keyframe.lua")
+include("skeleton/transform.lua")
+
+function load_example()
+
+    core = Bone:new(
+        "core",         -- aka hips
+        Vec:new(0, -4), -- points from hip to skull
+        Transform:new(
+            Vec:new(0, -12),    -- hips are 12 units above the ground
+            0                   -- no rotation
+        ),
+        0               -- default depth
+    )
+
+    skeleton = Skeleton:new(core, true)
+
+    return skeleton
+end
