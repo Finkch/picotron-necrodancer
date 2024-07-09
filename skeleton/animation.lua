29487e4b5e3903dbcc75ab5607148bd7e2c08b87
Finkch <skyler@thealdersons.org>
2024-07-09 15:57:58 -0700

    Indexing animation returns previous + next keyframes and progress between them

--- skeleton/animation.lua
+++ skeleton/animation.lua
@@ -18,3 +18,19 @@ function Animation:new(keyframes)
     setmetatable(a, Animation)
     return a
 end
+
+
+
+-- metamethods
+function Animation:__index(frame)   -- returns the two animations nearest to the frame
+    if (type(frame) != "number") return self[frame] -- doesn't override metatable
+    
+    for i = 1, #self.keyframes do
+        if (frame >= self.keyframes[i].frame) then
+            local k1 = self.keyframes[i]                            -- current key frame
+            local k2 = self.keyframes[(i + 1) % self.keyframes]     -- gets next keyframe
+            local progress = (f - k1.frame) / (k2.frame - k1.frame) -- progress to next frame
+            return k1, k2, progress                                 -- returns both frames and progress
+        end
+    end
+end
