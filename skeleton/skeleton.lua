610586cecb78e8f078d168bb8cb55acf2c145031
Finkch <skyler@thealdersons.org>
2024-07-09 13:08:38 -0700

    Added a skeleton to Skeleton (heh)

---
+++ skeleton/skeleton.lua
@@ -0,0 +1,18 @@
+--[[
+    a class for the heirarchy of bones and their skins.
+
+    hm, i would have expected the joint/bone to hold the skin information...
+
+]]
+
+Skeleton = {}
+Skeleton.__index = Skeleton
+Skeleton.__type = "skeleton"
+
+function Skeleton:new(model_data)
+
+    -- root joint
+
+    -- skin map
+
+end
