dfa1b5fa444384669673982e5eec196657fe55f2
Finkch <skyler@thealdersons.org>
2024-07-09 13:39:13 -0700

    Added attributes to Bone

--- skeleton/bone.lua
+++ skeleton/bone.lua
@@ -7,16 +7,12 @@ Bone = {}
 Bone.__index = Bone
 Bone.__type = "bone"
 
-function Bone:new()
-    local b  ={
-
+function Bone:new(name, transform)
+    local b = {
+        name = name,
+        children = {},
+        transform = transform -- current position, relative to model default
     }
-
-    -- id
-
-    -- children array
-
-    -- current transform (to model default)
 
     -- skin?
 
