1ee4c26b880584bba4daa1261318d85fdca620db
Finkch <skyler@thealdersons.org>
2024-07-09 16:07:02 -0700

    Added a set method for new animation

--- skeleton/necromancer.lua
+++ skeleton/necromancer.lua
@@ -12,9 +12,18 @@ Necromancer.__type = "necromancer"
 function Necromancer:new(animation)
     local n = {
         current = animation,    -- current animation
+        previous = nil          -- previous animation
         frame = 0               -- frame/time
     }
 
     setmetatable(n, Necromancer)
     return n
+end
+
+
+-- sets new animation
+function Necromancer:set(animation)
+    self.previous = self.current
+    self.current = animation
+    self.frame = 0
 end
