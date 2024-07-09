80bdd1bbe61fee0c715d8927d1b1a62e5433ccd7
Finkch <skyler@thealdersons.org>
2024-07-09 14:03:17 -0700

    Skeleton can now find its bones

--- skeleton/skeleton.lua
+++ skeleton/skeleton.lua
@@ -9,15 +9,26 @@ Skeleton = {}
 Skeleton.__index = Skeleton
 Skeleton.__type = "skeleton"
 
-function Skeleton:new(model_data)
+function Skeleton:new(root)
     local s = {
-
+        root = root,
+        bones = {}
     }
-
-    -- root joint
 
     -- skin map
 
     setmetatable(s, Skeleton)
     return s
+end
+
+-- sets the list of bones, recursing down children
+function Skeleton:findbones()
+    self.bones[self.root.name] = self.root
+    self:_findbones(self.root) 
+end
+
+function Skeleton:_findbones(current_bone)
+    for name, bone in pairs(current_bone.children) do
+        self.bones[name] = bone
+    end
 end
