b05bd6a64d56dbc40979440b0bb699700875e9a9
Finkch <skyler@thealdersons.org>
2024-07-09 19:37:05 -0700

    Updated graveyard for new example

--- graveyard.lua
+++ graveyard.lua
@@ -17,61 +17,59 @@ include("skeleton/transform.lua")
 function load_example()
 
     local core = Bone:new(
-        "core",         -- aka hips
-        Vec:new(0, -8), -- points from hip to skull
-        Transform:new(
-            Vec:new(0, -12),    -- hips are 12 units above the ground
-            0                   -- no rotation
-        ),
-        0               -- default depth
+        "core",             -- aka hips
+        Vec:new(0, -8),     -- points from hip to skull
+        0,                  -- default depth
+        Transform:new(),
+        Vec:new(0, -12)     -- 12 units off the ground
     )
 
     local rleg = Bone:new(
         "right leg",
         Vec:new(0, 6),
-        Transform:new(
-            core.transform.pos + Vec:new(-3, 1),
-            0
-        ),
-        1
+        1,
+        Transform:new(),
+        Vec:new(-4, 9)
     )
 
     local rforeleg = Bone:new(
         "right foreleg",
         Vec:new(0, 6),
-        Transform:new(
-            rleg:tip() + Vec:new(0, 1),
-            0
-        ),
         2
     )
+    rleg:add(rforeleg)
+    core:add(rleg)
 
+    
     local lleg = Bone:new(
         "left leg",
         Vec:new(0, 6),
-        Transform:new(
-            core.transform.pos + Vec:new(3, 1),
-            0.05
-        ),
-        1
+        1,
+        Transform:new(),
+        Vec:new(4, 9)
     )
 
     local lforeleg = Bone:new(
         "left foreleg",
         Vec:new(0, 6),
-        Transform:new(
-            lleg:tip() + Vec:new(0, 1),
-            0
-        ),
         2
     )
-
-    rleg:add(rforeleg)
+    
     lleg:add(lforeleg)
-    core:add(rleg)
     core:add(lleg)
+    
+    
 
     local skeleton = Skeleton:new(core, true)
 
     return skeleton
+end
+
+function load_example_pose()
+    local pose = {}
+
+    pose["right leg"] = -0.12
+    pose["right foreleg"] = 0.12
+
+    return pose
 end
