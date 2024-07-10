0d49459ffb3ee530aba62616f148904666b5a542
Finkch <skyler@thealdersons.org>
2024-07-10 16:11:15 -0700

    Animation no longer finds progress, key frames

--- skeleton/animation.lua
+++ skeleton/animation.lua
@@ -29,16 +29,3 @@ function Animation:findduration()
     end
     self.duration = duration
 end
-
-
--- metamethods
-function Animation:get(frame)   -- returns the two animations nearest to the frame
-    for i = 1, #self.keyframes do
-        if (frame >= self.keyframes[i].frame) then
-            local k1 = self.keyframes[i]                            -- current key frame
-            local k2 = self.keyframes[i % #self.keyframes + 1]      -- gets next keyframe
-            local progress = (frame - k1.frame) / (k2.frame - k1.frame) -- progress to next frame
-            return k1, k2, progress                                 -- returns both frames and progress
-        end
-    end
-end
