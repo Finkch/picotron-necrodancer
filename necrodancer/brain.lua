a3f9a4f8fb5d5cb256b3d153904f0b6972dcd833
Finkch <skyler@thealdersons.org>
2024-07-16 10:38:42 -0700

    Added generic Brain

--- necrodancer/brain.lua
+++ necrodancer/brain.lua
@@ -6,26 +6,50 @@
 
 
 --[[
+    generic brain
+
+]]
+
+Brain = {}
+Brain.__index = Brain
+Brain.__type = "brain"
+Brain.__parenttype = "brain"
+
+function Brain:new(source)
+    local b = {
+        source = source
+    }
+
+    setmetatable(b, Brain)
+    return b
+end
+
+-- empty update functions. Must be overridden.
+function Brain:update(gui) end
+
+
+
+
+--[[
     labelbrains grab data from a slider (or from anything with a :get())
     and push it into the label's readout
 
 ]]
-    
+
 LabelBrain = {}
 LabelBrain.__index = LabelBrain
 LabelBrain.__type = "labelbrain"
+setmetatable(LabelBrain, Brain)
 
 function LabelBrain:new(source, target)
-    local lb = {
-        source = source,
-        target = target
-    }
+    local lb = Brain:new(source)
+    lb.target = target
 
     setmetatable(lb, LabelBrain)
     return lb
 end
 
-function LabelBrain:update()
+function LabelBrain:update(gui)
     local datum = self.source:get()
 
     if (type(datum) == "number") datum = tostr(flr(rnd(datum)))
