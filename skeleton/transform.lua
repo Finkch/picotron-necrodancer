4306afb17111309c06ee4e455dd982334112edb2
Finkch <skyler@thealdersons.org>
2024-07-19 12:59:24 -0700

    Changed order of if-else to prevent index error

--- skeleton/transform.lua
+++ skeleton/transform.lua
@@ -3,12 +3,14 @@
 
 ]]
 
+include("lib/vec.lua")
+
 Transform = {}
 Transform.__index = Transform
 Transform.__type = Transform
 
 function Transform:new(pos, rot)
-    pos = pos or {x = 0, y = 0}
+    pos = pos or Vec:new()
     rot = rot or 0
     local t = {
         pos = pos,
@@ -30,14 +32,14 @@ end
 
 -- transforms a vector
 function Transform:__mul(other)
-    if (other.__type == "vec") then
-        return other:rotate(self.rot) + self.pos
-
-    elseif (type(other) == "number") then
+    if (type(other) == "number") then
         return Transform:new(
             self.pos * other,
             self.rot * other
         )
+
+    elseif (other.__type == "vec") then
+        return other:rotate(self.rot) + self.pos
 
     else
         error("cannot multiply transform with type \"" .. type(other) .. "\" (" .. tostr(other.__type) .. ")")
