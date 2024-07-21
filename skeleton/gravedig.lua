f99c6189f971f5bc2ef3b845aece22b5909130a0
Finkch <skyler@thealdersons.org>
2024-07-21 15:52:59 -0700

    gravedig now has functions to import skeleton

--- skeleton/gravedig.lua
+++ skeleton/gravedig.lua
@@ -41,3 +41,62 @@ function import(skeleton)
 
     return skeleton
 end
+
+
+
+
+
+-- recursively makes bones from the pod
+function getbones(bonetbl, parent)
+    local bone = Bone:new(
+        bonetbl.name,
+        Vec:new(bonetbl.bone.x, bonetbl.bone.y),
+        bonetbl.z,
+        Vec:new(bonetbl.joint.x, bonetbl.joint.y)
+    )
+    
+    for _, childtbl in pairs(bonetbl.children) do
+
+        local child = getbones(childtbl, bone)
+
+        if (parent) parent:add(child)
+    end
+
+    return bone
+end
+
+
+-- makes the necromancer from the pod
+function getnecromancer(necromancertbl)
+    local animations = {}
+    for name, animationtbl in pairs(necromancertbl) do
+        animations[name] = getanimation(animationtbl)
+    end
+
+    return Necromancer:new(animations)
+end
+
+function getanimation(animationtbl)
+    local keyframes = {}
+    for i = 1, #animationtbl.keyframes do
+        keyframes[i] = getkeyframe(animationtbl.keyframes[i])
+    end
+
+    return Animation:new(animationtbl.name, keyframes)
+end
+
+function getkeyframe(keyframetbl)
+    local transforms = {}
+    for bone, transformtbl in pairs(keyframetbl.transforms) do
+        transforms[bone] = gettransform(transformtbl)
+    end
+
+    return Keyframe:new(keyframetbl.duration, transforms)
+end
+
+function gettransform(transformtbl)
+    return Transform:new(
+        Vec:new(transformtbl.pos.x, transformtbl.pos.y),
+        transformtbl.rot
+    )
+end
