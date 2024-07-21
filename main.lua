e0d9cd1a4e5f1bb01a904d23e17e5966e7707229
Finkch <skyler@thealdersons.org>
2024-07-21 13:50:21 -0700

    Updated imports

--- main.lua
+++ main.lua
@@ -1,12 +1,5 @@
 --[[pod_format="raw",created="2024-07-09 17:25:38",modified="2024-07-15 18:02:22",revision=277]]
 
--- includes finkchlib and lib
-rm("/ram/cart/finkchlib") -- makes sure at most one copy is present
-mount("/ram/cart/finkchlib", "/ram/finkchlib")
-
-rm("/ram/cart/lib") -- makes sure at most one copy is present
-mount("/ram/cart/lib", "/ram/lib")
-
 
 include("necrodancer.lua")
 
